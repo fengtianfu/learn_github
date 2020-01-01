e491dc8ed3131fe71df154b5c0267ebb7eb35a32
zhang <shlzxcmath@outlook.com>
2020-01-01 13:33:01 +0800

    after render

---
+++ docs/_build.sh
@@ -0,0 +1,10 @@
+
+
+#!/bin/bash
+
+set -ev
+
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+
